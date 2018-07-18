39eff7de69b69e8d4b3657a151553b11507a789a
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-17 23:54:52 -0300

    ESLint

--- src/index.js
+++ src/index.js
@@ -5,24 +5,25 @@ import Button from './Button';
 import './style.scss';
 
 class App extends Component {
+  state = {
+    counter: 0,
+  };
 
-    state = {
-        counter: 0,
-    };
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
 
-    handleClick = () => {
-        this.setState({ counter: state.counter + 1 });
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <h1 className="title">Hello</h1>
-                <h2 style={{ color: "#f00" }}>{this.state.counter}</h2>
-                <Button onClick={this.handleClick} />
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <h1 className="title">Hello</h1>
+        <h2 style={{ color: "#f00" }}>{this.state.counter}</h2>
+        <Button onClick={this.handleClick} />
+      </Fragment>
+    )
+  }
 }
 
 render(<App initialCounter={3} />, document.getElementById('app'))
