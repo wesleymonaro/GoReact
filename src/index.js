bd31c6e9976e463da281d69f965fa6a5bb69d4f2
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-17 23:22:53 -0300

    Ciclo de vida

--- src/index.js
+++ src/index.js
@@ -8,6 +8,46 @@ class App extends Component {
         counter: 0,
     };
 
+    //Montagem/Atualização
+    //Executado antes de tudo
+    //Transforma props em estado
+    static getDerivedStateFromProps(nextProps, prevState) {
+        return { counter: nextProps.initialCounter }
+    }
+
+    //Montagem
+    //Executado quando o componente é renderizado em tela
+    componentDidMount() { }
+
+
+    // Atualizações
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.counter < 10
+    }
+
+    //Somente é executado caso o shouldComponentUpdate retorna true
+    //Serve para coletar alguma informação antes de atualizar o componente em tela
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        return prevState.counter;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(prevState, snapshot)
+    }
+
+
+
+    // Desmontagem
+    //Executa quando o componente vai deixar de existir
+    componentWillUnmount() { }
+
+
+    //ERROR
+    //Sempre que acontece um erro
+    componentDidCatch(error, info) {
+        console.log('Erro ', error)
+    }
+    
 
     handleClick = () => {
         // COMO CALLBACK
@@ -16,8 +56,8 @@ class App extends Component {
         // })
 
         //EM FILA
-        this.setState(state => ({ counter: state.counter + 1}));
-        this.setState(state => ({ counter: state.counter + 1}));
+        this.setState(state => ({ counter: state.counter + 1 }));
+        this.setState(state => ({ counter: state.counter + 1 }));
     }
 
     render() {
@@ -30,4 +70,4 @@ class App extends Component {
     }
 }
 
-render(<App />, document.getElementById('app'))
+render(<App initialCounter={3} />, document.getElementById('app'))
