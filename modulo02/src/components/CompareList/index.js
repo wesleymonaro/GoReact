d16a76f9e066d98f9da05c317253f8efb7e41faf
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-24 13:25:55 -0300

    Enviando repositórios com props

--- modulo02/src/components/CompareList/index.js
+++ modulo02/src/components/CompareList/index.js
@@ -2,76 +2,36 @@ import React from 'react';
 
 import { Container, Repository } from './styles'
 
-const CompareList = () => (
+const CompareList = ({repositories}) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
+    {
+      repositories.map(repository => (
+        <Repository key={repository.id}>
+          <header>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <strong>{repository.owner.login}</strong>
+            <small>{repository.name}</small>
+          </header>
 
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+          <ul>
+            <li>
+            {repository.stargazers_count} <small>stars</small>
+            </li>
+            <li>
+            {repository.forks_count} <small>forks</small>
+            </li>
+            <li>
+            {repository.open_issues_count} <small>issues</small>
+            </li>
+            <li>
+            {repository.pushed_at} <small>last commit</small>
+            </li>
+          </ul>
+        </Repository>
+      ))
+    }
 
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
 
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
   </Container>
 )
 
