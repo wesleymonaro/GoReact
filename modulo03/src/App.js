7118f0826cea1bb1a0d4c6127b08566dee8895c9
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-04 11:47:52 -0300

    Criando Routes

--- modulo03/src/App.js
+++ modulo03/src/App.js
@@ -1,13 +1,14 @@
 import React  from 'react';
-
 import { Provider } from 'react-redux';
+
 import './config/reactotron';
 import store from './store';
-import TodoList from './TodoList';
+
+import Routes from './routes';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <Routes />
   </Provider>
 );
 
