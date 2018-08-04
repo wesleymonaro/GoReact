f9847df52785e56fc7f0924ae6448b7c72183a0d
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-04 11:12:49 -0300

    Lista de todos

---
+++ modulo03/src/store/reducers/todos.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = [
+  {id: 1, text: 'Fazer café'},
+  {id: 1, text: 'Estudar react'}
+]
+
+export default function todos(state = INITIAL_STATE, action){
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+}
