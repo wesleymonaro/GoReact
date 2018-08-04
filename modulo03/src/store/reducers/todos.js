2f89113e096fb205c6d887d5ec1ea1f191529f1e
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-04 11:20:53 -0300

    Adicionar todo

--- modulo03/src/store/reducers/todos.js
+++ modulo03/src/store/reducers/todos.js
@@ -1,10 +1,12 @@
 const INITIAL_STATE = [
   {id: 1, text: 'Fazer café'},
-  {id: 1, text: 'Estudar react'}
+  {id: 2, text: 'Estudar react'}
 ]
 
 export default function todos(state = INITIAL_STATE, action){
   switch (action.type) {
+    case 'ADD_TODO':
+      return [ ...state, { id: Math.random(), text: action.payload.text }]
 
     default:
       return state;
