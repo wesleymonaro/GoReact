59b7043fa41c7dceedcb69e9388c0e3ef58da782
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-13 19:41:15 -0300

    Estilizando página Playlist

--- modulo04/src/pages/browse/index.js
+++ modulo04/src/pages/browse/index.js
@@ -12,25 +12,25 @@ const Browse = () => (
     <Title>Navegar</Title>
 
     <List>
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img src="https://pbs.twimg.com/profile_images/1160471124/profile_logo_400x400.jpg" alt="Cover" />
         <strong>Rock dos bons</strong>
         <p>Relaze enquantovc programa ouvindo o melhor do rock</p>
       </Playlist>
 
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img src="https://pbs.twimg.com/profile_images/1160471124/profile_logo_400x400.jpg" alt="Cover" />
         <strong>Rock dos bons</strong>
         <p>Relaze enquantovc programa ouvindo o melhor do rock</p>
       </Playlist>
 
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img src="https://pbs.twimg.com/profile_images/1160471124/profile_logo_400x400.jpg" alt="Cover" />
         <strong>Rock dos bons</strong>
         <p>Relaze enquantovc programa ouvindo o melhor do rock</p>
       </Playlist>
 
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img src="https://pbs.twimg.com/profile_images/1160471124/profile_logo_400x400.jpg" alt="Cover" />
         <strong>Rock dos bons</strong>
         <p>Relaze enquantovc programa ouvindo o melhor do rock</p>
