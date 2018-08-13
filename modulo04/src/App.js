6c37156a99644fe27a710ea7df41dfe324003c6d
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-13 18:57:00 -0300

    Estilizando Header

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -4,13 +4,17 @@ import './styles/global';
 
 import Sidebar from './components/sidebar';
 import Player from './components/player';
+import Header from './components/header';
 
-import { Wrapper, Container } from './styles/components'
+import { Wrapper, Container, Content } from './styles/components'
 
 const App = () => (
   <Wrapper>
     <Container>
       <Sidebar />
+      <Content>
+        <Header/>
+      </Content>
     </Container>
     <Player />
   </Wrapper>
